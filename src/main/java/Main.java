ff68382de32e21b82aa19ee5e05300cdc576a76f
Shakirzyanov Arsen <arsen.shakirzyanov@mail.ru>
2018-04-19 00:56:22 +0500

    Main functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Solver solver = new Solver();
+        int result = solver.solve(a, b);
+        System.out.println(result);
     }
 }
